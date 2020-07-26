c00db620078578ee2cb32b158585be3ca1c101f7
yuchen <smirkiiiv@gmail.com>
2020-07-26 15:28:17 +0800

    Initial Commit

---
+++ passCodeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  passCodeDemo
+//
+//  Created by 林祐辰 on 2020/7/26.
+//  Copyright © 2020 smirkiiiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
